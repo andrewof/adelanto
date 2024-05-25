86ca814af80c899a569a1398facdb879a3f74b86
andrewof <141205481+andrewof@users.noreply.github.com>
2024-05-24 22:11:35 -0500

    adelanto - home cliente

--- frontend/src/components/Clientes/SectionSecundario/SectionSecundario.js
+++ frontend/src/components/Clientes/SectionSecundario/SectionSecundario.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { Grid, Header, Icon, Button, Segment, Image } from "semantic-ui-react";
+import image from "../../../media/media-home-clientes/atencion1.png";
 import "./SectionSecundario.scss";
 
 export function SectionSecundario() {
   return (
-    <div>SectionSecundario</div>
+    <Segment raised className="custom-segment" style={{ padding: '2em', borderRadius: '10px', display: 'flex', alignItems: 'center' }}>
+      <Grid>
+        <Grid.Row columns={2}>
+          <Grid.Column width={4} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', background: '#1a2c38', borderRadius: '10px' }}>
+            <div style={{ textAlign: 'center' }}>
+              <Image src={image} className="segment-image"/>
+            </div>
+          </Grid.Column>
+          <Grid.Column width={12} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', paddingLeft: '2em' }}>
+            <Header as='h2' className="segment-title">Atendemos tus necesidades durante cualquier hora del día.</Header>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
   )
 }
